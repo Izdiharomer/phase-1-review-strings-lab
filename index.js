1fbcc06a10e2e5fd69d29298eed2332e0cc6dda1
Izdihar <ezdiharomerrrr@gmail.com>
2023-04-24 16:50:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+console.log(welcomeMessage);
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toLocaleUpperCase()}!`;
+console.log(excitedWelcomeMessage);
+
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+
+console.log(shortGreeting);
